622ee15332433485a393b7b6dcde57687618cdbe
lapplislazuli <Leonhard.Applis@Protonmail.com>
2025-07-24 10:21:18 +0800

    Added a small cache to stop microagents from loading multiple times

--- useagent/microagents/management.py
+++ useagent/microagents/management.py
@@ -3,6 +3,9 @@ from pathlib import Path
 import useagent
 from useagent.microagents.microagent import MicroAgent, load_microagent
 
+# Simple `Cache` that we only load things once. There should never be changes to the MicroAgents during Runtime.
+_dict_microagents: dict[str, list[MicroAgent]] = {}
+
 
 def _get_project_root() -> Path:
     return Path(useagent.__file__).resolve().parents[1]
@@ -13,10 +16,14 @@ def _get_default_microagent_directory() -> Path:
 
 
 def load_microagents(dir_path: str) -> list[MicroAgent]:
+    if dir_path in _dict_microagents.keys():
+        return _dict_microagents[dir_path]
+
     results: list[MicroAgent] = []
     for path in Path(dir_path).rglob("*.microagent.md"):
         if path.name.count(".") >= 2 and path.name.endswith(".microagent.md"):
             results.append(load_microagent(path))
+    _dict_microagents[dir_path] = results
     return results
 
 
