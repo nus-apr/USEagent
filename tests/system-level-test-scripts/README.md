26263066623b41eea0ea3aaa98d270f4a4e0f3bc
lapplislazuli <Leonhard.Applis@Protonmail.com>
2025-08-08 10:42:28 +0800

    Made a folder for bug-reproducing scripts, one issue instance that timeouts on install

---
+++ tests/system-level-test-scripts/README.md
@@ -0,0 +1,10 @@
+# System Level Tests
+
+In this folder are `informal` tests that help to reproduce some of the issues we see from our experiments. 
+They are not meant to be part of the CI/CD and run all the time, 
+but more to have an easy way to reproduce reported behaviour. 
+
+Each script gets a small header linking to issues and what was observed. 
+They are similar in nature to the scripts in `./scripts`. 
+
+With more programming efforts the issues might disappear and the scripts deprecate (i.e. their bug got fixed or changed)
---
+++ tests/system-level-test-scripts/test-github-dependency-install.sh
@@ -0,0 +1,40 @@
+# We have observed some issues that programs just `end` after an install command, not printing any errors. 
+# This might be related to docker issues, i.E. I don't see any exceptions from Useagent
+# One project where this was observed was Guava, which tried to install the jdk first. 
+# The behavior to be observed is that it should try to install java first, which might take a while. 
+
+# To run the script, you need a valid API key in .env, as well as docker up and running.
+
+set -a
+source .env
+set +a
+
+if [[ "$1" == "--build" ]]; then
+  docker rm -f useagent-turbo-test 2>/dev/null
+  if [[ "$2" == "--rm-image" ]]; then
+    #This will delete the image, but you will loose caching.
+    docker image rm -f useagent-turbo:dev 2>/dev/null
+  fi
+  DOCKER_BUILDKIT=1 docker build --ssh default -t useagent-turbo:dev .
+fi
+
+
+REPO_URL=https://github.com/google/guava.git
+MODEL_NAME=google-gla:gemini-2.0-flash
+
+mkdir ./tmp
+cat > ./tmp/task.md <<'EOF'
+You are a software engineering and now there is a project under current working directory. Your task is to produce a bash script named run_test.sh that can build the project and run the tests for this project.
+You are working on a ubuntu 22.04 platform. In case some projects require additional dependency, you can choose to install OS-level dependency as well.
+You need to always figure out what is the primary programming language this project is using, what build system does this project use and what testing framework in this project.
+You should execute your bash script to observe the results. If you think the output shows tests passed, you can terminate the workflow.
+EOF
+
+
+docker run --rm \
+  --name useagent-turbo-test \
+  -e GEMINI_API_KEY=$GEMINI_API_KEY \
+  -v ./tmp:/task:rw \
+  -v ./useagent-turbo-tmp-out:/output:rw \
+  useagent-turbo:dev \
+  /bin/bash -c "PYTHONPATH=. uv run python useagent/main.py github --model $MODEL_NAME --task-file /task/task.md --repo-url $REPO_URL  --output-dir /output"
