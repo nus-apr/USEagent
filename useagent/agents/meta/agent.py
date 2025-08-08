f2641e1e0a58c1fd3d4166f7878c46776d5e8e56
lapplislazuli <Leonhard.Applis@Protonmail.com>
2025-08-08 18:32:08 +0800

    Added a simple old-message-summarizer as per pydantic example

--- useagent/agents/meta/agent.py
+++ useagent/agents/meta/agent.py
@@ -4,6 +4,7 @@ from pathlib import Path
 
 from loguru import logger
 from pydantic_ai import Agent, RunContext
+from pydantic_ai.messages import ModelMessage
 from pydantic_ai.tools import Tool
 
 from useagent.agents.edit_code.agent import init_agent as init_edit_code_agent
@@ -36,6 +37,36 @@ SYSTEM_PROMPT = (Path(__file__).parent / "system_prompt.md").read_text()
 USAGE_TRACKER = UsageTracker()
 
 
+async def summarize_old_messages(messages: list[ModelMessage]) -> list[ModelMessage]:
+    # After hitting 100 messages, summarize the oldest 30
+    MESSAGE_THRESHHOLD: int = 100
+    SUMMARY_FRAME: int = 30
+    if len(messages) > MESSAGE_THRESHHOLD:
+        logger.info(
+            f"[META] Summary triggered, summarizing oldest {SUMMARY_FRAME} messages"
+        )
+        summarize_agent = Agent(
+            ConfigSingleton.config.model,
+            instructions="""
+        Summarize this conversation, omitting small talk and unrelated topics.
+        Focus on the technical discussion, most relevant artifacts and next steps.
+        Filter out noisy artifacts or information that is not relevant to the goals. 
+        Summarize elements that have only been part from a `checklist` but have been successfully checked.
+        """,
+        )
+
+        oldest_messages = messages[:SUMMARY_FRAME]
+        summary = await summarize_agent.run(message_history=oldest_messages)
+        USAGE_TRACKER.add("SUMMARIZER", summary.usage())
+        logger.debug(
+            f"[META] last {SUMMARY_FRAME} messages summarized as: \n {summary.new_messages()}"
+        )
+        # Return the last message and the summary
+        return summary.new_messages() + messages[-1:]
+
+    return messages
+
+
 @conditional_microagents_triggers(load_microagents_from_project_dir())
 @alias_for_microagents("META")
 def init_agent(config: AppConfig | None = None) -> Agent[TaskState, str]:
@@ -55,6 +86,7 @@ def init_agent(config: AppConfig | None = None) -> Agent[TaskState, str]:
             Tool(remove_diffs_from_diff_store, takes_ctx=True, max_retries=5),
         ],
         output_type=str,
+        history_processors=[summarize_old_messages],
     )
 
     ## This adds the task description to instructions (SYSTEM prompt).
